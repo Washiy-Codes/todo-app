98c3302e5b9be72e0214f43ab5d0ff72eed0ff8a
Washiy <wachira.wangui2023@students.jkuat.ac.ke>
2025-11-02 17:41:18 +0300

    Updated Todo app UI and Logics

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -10,29 +10,42 @@ const Todo = () => {
     };
 
     const handleSubmit = () => {
-        if (input.trim() === "") return;
-        setTodos([...todos, input]);
-        setInput("");
+        if(input) {
+            setTodos([...todos, input]);
+            setInput("");
+        }
     }
 
     const handleDelete = (index) => {
-        const newTodos = todos.filter((_, i) => i !== index);
-        setTodos(newTodos);
+       const newTodos = todos.filter((i) => i !== todos[index]);
+       setTodos(newTodos);
+
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSubmit();
+        }
     }
 
   return (
     <div className="todo-container">
+      <div className="input-group">
         <input 
           type="text" 
           value={input} 
           onChange={handleChange} 
           placeholder="Add a new todo" 
+          onKeyDown={handleKeyDown}
         />
         <button type="button" onClick={handleSubmit} className="btn">Add Todo</button>
+      </div>
         <ul>
+            {todos.length === 0 && <p>No todos available. Please add some tasks!</p>}
             {todos.map((todo, index) => (
                 <li key={index}>
-                    <span>{todo}</span><button type="button" onClick={() => handleDelete(index)}>Delete</button>
+                    <span>{todo}</span>
+                    <button type="button" onClick={() => handleDelete(index)}>Delete</button>
                 </li>
             ))}
         </ul>
